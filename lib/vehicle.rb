0f83b6eec854569a206ed1fbc5862d4889fcb7fa
github username <github email address>
2018-02-02 12:30:33 -0500

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,14 @@
 class Vehicle
+  attr_accessor :wheel_size, :number, :wheel_number
+  def initialize(wheel_size, number)
+
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+  def fill_up_tank
+    "filling up!"
+  end
 
 end
